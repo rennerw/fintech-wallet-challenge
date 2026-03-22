2da2a20ff8accbab64b71e3d8c19d24ac90fbe48
Wellinghton <wrsilva.terceiro@brasilcap.com.br>
2026-03-22 02:57:15 -0300

    ci: so estava iniciando o node

--- docker/start.sh
+++ docker/start.sh
@@ -5,5 +5,6 @@ php artisan migrate:reset --force
 php artisan migrate --force
 php artisan db:seed --class=DatabaseSeeder --force
 
-npm run dev -- --host 0.0.0.0 --port 5173 &
-exec php artisan serve --host=0.0.0.0 --port="${PORT:-8000}"
+npm run dev -- --host 0.0.0.0 --port 5173 & \
+php artisan serve --host=0.0.0.0 --port="${PORT:-8000}" &
+wait -n
